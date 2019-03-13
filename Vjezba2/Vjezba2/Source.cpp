ea678fdca8395223f98b1b7bddde1617c0599af5
m-luketin <ml46019@oss.unist.hr>
2019-03-13 01:11:35 +0100

    Finished the first two tasks

---
+++ Vjezba2/Vjezba2/Source.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int* new_array(int array_size) 
+{
+	int *array = new int[array_size];
+
+	cout << "Enter array elements:\n";
+	for (int i = 0; i < array_size - 1; ++i)
+		cin >> array[i];
+
+	array[array_size - 1] = 0;
+	return array;
+}
+
+bool is_number_in_array(int* array, int array_size, int number_for_search)
+{
+	for (int i = 0; i < array_size; i++)
+	{
+		if (array[i] == number_for_search)
+			return true;
+	}
+	return false;
+}
+
+int* add_missing_element(int* array, int array_size) 
+{
+	for (int i = 1; i < array_size; i++)
+	{
+		if (!is_number_in_array(array, array_size, i))
+		{
+			array[array_size - 1] = i;
+			break;
+		}
+	}
+
+	sort(array, array + array_size);
+	return array;
+}
+
+int main()
+{
+	int size_of_array = 9;
+	int* array = new_array(size_of_array);
+	
+	cout << "\nEntered array:\n";
+	for (int i = 0; i < size_of_array - 1; i++) 
+	{
+		cout << array[i] << " ";
+	}
+	cout << "\n";
+
+	sort(array, array + size_of_array - 1);
+
+	array = add_missing_element(array, size_of_array);
+
+	cout << "\nSorted with missing element:\n";
+	for (int i = 0; i < size_of_array; i++) 
+	{
+		cout << array[i] << " ";
+	}
+	cout << "\n";
+
+	system("pause");
+}
---
+++ Vjezba2/task2/Source.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int* new_array(int array_size)
+{
+	int *array = new int[array_size];
+	
+	cout << "Enter array elements:\n";
+	for (int i = 0; i < array_size; ++i)
+		cin >> array[i];
+	
+	
+	return array;
+}
+
+bool is_odd(int* n) 
+{
+	return(*n % 2);
+}
+
+bool is_even(int* n)
+{
+	return(!(*n % 2));
+}
+
+void swap(int* a, int* b) 
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void print(int* array, int array_size)
+{
+	for (int i = 0; i < array_size; i++) {
+		cout << array[i] << " ";
+	}
+	cout << "\n";
+}
+
+void parity_sort(int* array, int array_size) 
+{
+	int *start = array, *end = array + array_size - 1;
+
+	while (start != end + 1) 
+	{
+		if (is_odd(start) && is_even(end))
+			swap(start, end);
+
+		while(is_even(start))
+			start++;
+
+		while(is_odd(end))
+			end--;
+	}
+}
+int main() 
+{
+	int size_of_array = 8;
+	int* array = new_array(size_of_array);
+
+	parity_sort(array, size_of_array);
+
+	print(array, size_of_array);
+
+	system("pause");
+}
