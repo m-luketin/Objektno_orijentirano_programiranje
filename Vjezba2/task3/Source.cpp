59e9b61d524b74ca7e832a5c982bc245d2d608ed
m-luketin <ml46019@oss.unist.hr>
2019-03-14 17:25:02 +0100

    Made the task 3 functions methods

--- Vjezba2/task3/Source.cpp
+++ Vjezba2/task3/Source.cpp
@@ -9,110 +9,115 @@ struct fake_vector
 	int* array;
 	int allocated;
 	int size;
+
+	void vector_new(int allocation_size)
+	{
+		fake_vector* vector = new fake_vector;
+
+		if (!allocation_size) {
+			array = new int[init];
+			allocated = init;
+		}
+		else {
+			array = new int[allocation_size];
+			allocated = allocation_size;
+		}
+
+		size = 0;
+	}
+
+	void vector_delete()
+	{
+		delete[] array;
+		size = NULL;
+		allocated = NULL;
+	}
+
+	int vector_size()
+	{
+		return size;
+	}
+
+	int* vector_front()
+	{
+		return array;
+	}
+
+	int* vector_back()
+	{
+		return (&array[size - 1]);
+	}
+
+	void double_allocated_memory()
+	{
+		int* tmp_array = new int[allocated];
+
+		for (int i = 0; i < size; i++)
+			tmp_array[i] = this->array[i];
+
+		delete[] array;
+		array = new int[allocated * 2];
+
+		for (int i = 0; i < size; i++)
+			this->array[i] = tmp_array[i];
+
+		delete[] tmp_array;
+	}
+
+	void vector_push_back(int number_to_push)
+	{
+		if (size == allocated)
+			this->double_allocated_memory();
+
+		array[size] = number_to_push;
+		size++;
+	}
+
+	int* vector_pop_back()
+	{
+		size--;
+		return &array[size];
+	}
+
+	void vector_print()
+	{
+		for (int i = 0; i < size; i++)
+			cout << array[i] << " ";
+		cout << "\n";
+	}
 };
-
-fake_vector* vector_new(int allocation_size)
-{
-	fake_vector* vector = new fake_vector;
-
-	if (!allocation_size) {
-		vector->array = new int[init];
-		vector->allocated = init;
-	}
-	else {
-		vector->array = new int[allocation_size];
-		vector->allocated = allocation_size;
-	}
-
-	vector->size = 0;
-	return vector;
-}
-
-void vector_delete(fake_vector* vector)
-{
-	delete[] vector->array;
-	delete vector;
-}
-
-int vector_size(fake_vector* vector)
-{
-	return vector->size;
-}
-
-int* vector_front(fake_vector* vector)
-{
-	return (vector->array);
-}
-
-int* vector_back(fake_vector* vector)
-{
-	return (&(vector->array[vector->size - 1]));
-}
-
-fake_vector* double_allocated_memory(fake_vector* vector)
-{
-	fake_vector* tmp_vector = vector_new(vector->allocated * 2);
-
-	for (int i = 0; i < vector->size; i++)
-		tmp_vector->array[i] = vector->array[i];
-
-	tmp_vector->size = vector->size;
-	vector_delete(vector);
-	return tmp_vector;
-}
-
-void vector_push_back(fake_vector** vector, int number_to_push)
-{
-	if ((*vector)->size == (*vector)->allocated)
-		(*vector) = double_allocated_memory(*vector);
-
-	(*vector)->array[(*vector)->size] = number_to_push;
-	(*vector)->size++;
-}
-
-int* vector_pop_back(fake_vector* vector)
-{
-	vector->size--;
-	return &(vector->array[vector->size]);
-}
-
-void vector_print(fake_vector* vector)
-{
-	for (int i = 0; i < vector->size; i++)
-		cout << vector->array[i] << " ";
-	cout << "\n";
-}
 
 int main()
 {
-	fake_vector *vector = vector_new(NULL);
+	fake_vector vector;
+	vector.vector_new(NULL);
 
 	int tmp;
-	for (int i = 0; i < vector->allocated; i++)
+	for (int i = 0; i < vector.allocated; i++)
 	{
 		cin >> tmp;
-		vector_push_back(&vector, tmp);
+		vector.vector_push_back(tmp);
 	}
 
-	vector_print(vector);
-	cout << "Vector size: " << vector_size(vector) << "\n";
-	cout << "Vector_front: " << *vector_front(vector) << "\n";
-	cout << "Vector_back: " << *vector_back(vector) << "\n\n";
+	vector.vector_print();
+	cout << "Vector size: " << vector.vector_size() << "\n";
+	cout << "Vector_front: " << *vector.vector_front() << "\n";
+	cout << "Vector_back: " << *vector.vector_back() << "\n\n";
 
-	vector_push_back(&vector, 20);
-	vector_print(vector);
-	cout << "Vector size: " << vector_size(vector) << "\n";
-	cout << "Vector_front: " << *vector_front(vector) << "\n";
-	cout << "Vector_back: " << *vector_back(vector) << "\n\n";
+	vector.vector_push_back(20);
+	vector.vector_print();
+	cout << "Vector size: " << vector.vector_size() << "\n";
+	cout << "Vector_front: " << *vector.vector_front() << "\n";
+	cout << "Vector_back: " << *vector.vector_back() << "\n\n";
 
-	cout << "Popped " << *vector_pop_back(vector) << " out\n";
-	cout << "Popped " << *vector_pop_back(vector) << " out\n\n";
+	cout << "Popped " << vector.vector_pop_back() << " out\n";
+	cout << "Popped " << vector.vector_pop_back() << " out\n\n";
 
-	vector_print(vector);
-	cout << "Vector size: " << vector_size(vector) << "\n";
-	cout << "Vector_front: " << *vector_front(vector) << "\n";
-	cout << "Vector_back: " << *vector_back(vector) << "\n\n";
+	vector.vector_print();
+	cout << "Vector size: " << vector.vector_size() << "\n";
+	cout << "Vector_front: " << *vector.vector_front() << "\n";
+	cout << "Vector_back: " << *vector.vector_back() << "\n\n";
 
-	vector_delete(vector);
+	vector.vector_delete();
 	system("pause");
 }
